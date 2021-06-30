f7506f7ff6c3879c801a4936aab09beb4d81ccc3
Mamatha-1257 <18wh1a1257@bvrithyderabad.edu.in>
2021-06-30 21:22:34 +0530

    Buttons added

--- SharedPreferences/MainActivity.java
+++ SharedPreferences/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.sharedpreferences;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private EditText name, age;
     private RadioButton radiomale, radiofemale;
+    private RadioGroup radioGroupGender;
     SharedPreferences prefs;
 
     @Override
@@ -21,48 +26,38 @@ public class MainActivity extends AppCompatActivity {
         age = findViewById(R.id.edit2);
         radiofemale = findViewById(R.id.radioFemale);
         radiomale = findViewById(R.id.radioMale);
+        radioGroupGender = findViewById(R.id.radioGroupGender);
         prefs = getSharedPreferences("MySharedPref", MODE_PRIVATE);
-
-        radiofemale.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean female_isChecked) {
-                genderSaveToSharedPreferences("gender_female", female_isChecked);
-            }
-        });
-
-        radiomale.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean male_isChecked) {
-                genderSaveToSharedPreferences("gender_male", male_isChecked);
-            }
-        });
-
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
+
+
+    public void saveContent(View view) {
+        if (name.getText().toString() == "" || age.getText().toString() == "") {
+            Toast.makeText(MainActivity.this, "Enter the details", Toast.LENGTH_SHORT).show();
+        } else {
+            SharedPreferences.Editor myEdit = prefs.edit();
+            myEdit.putString("name", name.getText().toString());
+            myEdit.putInt("age", Integer.parseInt(age.getText().toString()));
+            myEdit.putBoolean("gender_female", radiofemale.isChecked());
+            myEdit.putBoolean("gender_male", radiomale.isChecked());
+            myEdit.apply();
+        }
+    }
+    public void showContent(View view) {
+
         String s1 = prefs.getString("name", "");
         int a = prefs.getInt("age", 0);
+        boolean f = prefs.getBoolean("gender_female",false);
+        boolean m = prefs.getBoolean("gender_male",false);
         name.setText(s1);
         age.setText(String.valueOf(a));
-        radiofemale.setChecked(genderUpdate("gender_female"));
-        radiomale.setChecked(genderUpdate("gender_male"));
+        radioGroupGender.clearCheck();
+        radiofemale.setChecked(f);
+        radiomale.setChecked(m);
     }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        SharedPreferences.Editor myEdit = prefs.edit();
-        myEdit.putString("name", name.getText().toString());
-        myEdit.putInt("age", Integer.parseInt(age.getText().toString()));
-
-        myEdit.apply();
-    }
-    private void genderSaveToSharedPreferences(String key, boolean value){
-        SharedPreferences.Editor myEdit = prefs.edit();
-        myEdit.putBoolean(key,value);
-        myEdit.apply();
-    }
-    private boolean genderUpdate(String key){
-        return prefs.getBoolean(key,false);
-    }
-}
+    public void clearContent(View view){
+        name.setText("");
+        age.setText("");
+        radiofemale.setChecked(false);
+        radiomale.setChecked(false);
+    }}
