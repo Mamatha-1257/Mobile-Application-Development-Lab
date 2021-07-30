ef5389fc50ae595cd41248cb6a57c7c5b507f9ae
Mamatha-1257 <18wh1a1257@bvrithyderabad.edu.in>
2021-07-30 22:39:11 +0530

    Completed experiment 12

---
+++ Experiment 12/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.app12;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    TimePicker alarmTimePicker;
+    PendingIntent pendingIntent;
+    AlarmManager alarmManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        alarmTimePicker = (TimePicker) findViewById(R.id.timePicker);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+    }
+    public void OnToggleClicked(View view) {
+        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Date dat = new Date();
+        Calendar cal_alarm = Calendar.getInstance();
+        Calendar cal_now = Calendar.getInstance();
+        cal_now.setTime(dat);
+        cal_alarm.setTime(dat);
+        cal_alarm.set(Calendar.HOUR_OF_DAY, 14);
+        cal_alarm.set(Calendar.MINUTE, 18);
+        cal_alarm.set(Calendar.SECOND, 0);
+        if (cal_alarm.before(cal_now)) {
+            cal_alarm.add(Calendar.DATE, 1);
+        }
+
+        Intent myIntent = new Intent(this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, myIntent, 0);
+
+        manager.set(AlarmManager.RTC_WAKEUP, cal_alarm.getTimeInMillis(), pendingIntent);
+    }}
---
+++ Experiment 12/README.md
@@ -0,0 +1,1 @@
+12. Create an alarm that rings every Sunday at 8:00 AM. Modify it to use a time picker to set alarm time.
